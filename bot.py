41bf9ce5e5a1e263fd1237c3d55b6d078b26e41d
hardddtrip <hardddtrip@gmail.com>
2025-02-01 01:18:52 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 from telegram import Update
 from telegram.ext import ContextTypes
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_TOKEN = os.getenv.get("TELEGRAM_BOT_TOKEN")
 
 
 
